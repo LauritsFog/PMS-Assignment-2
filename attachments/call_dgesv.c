36b6d291624ac45ddc2969ade3e672ebbe95d195
LauritsFog <lause.fog@gmail.com>
2022-11-12 15:09:07 +0100

    main file

--- attachments/call_dgesv.c
+++ attachments/call_dgesv.c
@@ -1,3 +1,4 @@
+#include "call_dgesv.h"
 #include "datatypes.h"
 
 /* C prototype for LAPACK routine DGESV */
@@ -9,7 +10,7 @@ void dgesv_(const int *n,    /* columns/rows in A          */
             double *B,       /* array B                    */
             const int *ldb,  /* leading dimension of B     */
             int *info        /* status code                */
-            );
+);
 
 /* call_dgesv : wrapper for LAPACK's DGESV routine
 
@@ -26,40 +27,38 @@ following exceptions: the return value is
    -11 if the dimensions of A and b are incompatible
    -12 in case of memory allocation errors.
 */
-int call_dgesv(array2d_t * A, array_t * b) {
-  
-  if (A == NULL || b == NULL){
+int call_dgesv(array2d_t *A, array_t *b)
+{
+
+  if (A == NULL || b == NULL)
+  {
     return -9;
   }
 
-  if (A->shape[0] != A->shape[1]){
+  if (A->shape[0] != A->shape[1])
+  {
     return -10;
   }
 
-  if (A->shape[0] != b->len){
-   return -11;
+  if (A->shape[0] != b->len)
+  {
+    return -11;
   }
-
 
   int n = A->shape[0];
   int piv[n];
   int info;
   int nrhs = 1;
 
-  
-
-  dgesv_( &n,            // n 
-          &nrhs,        // nrhs 
-          A->val,       // A   
-          &n,           // lda 
-          piv,         // ipiv 
-          b->val,       // B 
-          &n,           // ldb 
-          &info         // info - ??
+  dgesv_(&n,     // n
+         &nrhs,  // nrhs
+         A->val, // A
+         &n,     // lda
+         piv,    // ipiv
+         b->val, // B
+         &n,     // ldb
+         &info   // info - ??
   );
 
-// Ingen ide om hvorfor det her lort ik virker...
-
-return info;
-
+  return 0;
 }
